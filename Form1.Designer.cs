84a271ca42cf01f8f45ba8a39160be425319b4e1
drago <drago@EM-LOPTAP>
2025-10-30 09:31:21 -0400

    Finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,40 +29,98 @@
         private void InitializeComponent()
         {
             this.grpQuestion = new System.Windows.Forms.GroupBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
-            this.radA = new System.Windows.Forms.RadioButton();
-            this.lblQuestion = new System.Windows.Forms.Label();
-            this.radB = new System.Windows.Forms.RadioButton();
-            this.radC = new System.Windows.Forms.RadioButton();
             this.radD = new System.Windows.Forms.RadioButton();
+            this.radC = new System.Windows.Forms.RadioButton();
+            this.radB = new System.Windows.Forms.RadioButton();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.radA = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.lblAnswer = new System.Windows.Forms.Label();
             this.lblAnswerLine2 = new System.Windows.Forms.Label();
             this.btnNext = new System.Windows.Forms.Button();
+            this.lblAnswerLine3 = new System.Windows.Forms.Label();
             this.grpQuestion.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpQuestion
             // 
             this.grpQuestion.BackColor = System.Drawing.Color.Transparent;
+            this.grpQuestion.Controls.Add(this.lblQuestion);
             this.grpQuestion.Controls.Add(this.radD);
             this.grpQuestion.Controls.Add(this.radC);
             this.grpQuestion.Controls.Add(this.radB);
-            this.grpQuestion.Controls.Add(this.lblQuestion);
             this.grpQuestion.Controls.Add(this.radA);
-            this.grpQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpQuestion.Font = new System.Drawing.Font("Mistral", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpQuestion.ForeColor = System.Drawing.SystemColors.Control;
             this.grpQuestion.Location = new System.Drawing.Point(42, 32);
             this.grpQuestion.Name = "grpQuestion";
-            this.grpQuestion.Size = new System.Drawing.Size(400, 379);
+            this.grpQuestion.Size = new System.Drawing.Size(400, 387);
             this.grpQuestion.TabIndex = 0;
             this.grpQuestion.TabStop = false;
             this.grpQuestion.Text = "Question 1";
             this.grpQuestion.Enter += new System.EventHandler(this.grpQuestion_Enter);
             // 
+            // radD
+            // 
+            this.radD.AutoSize = true;
+            this.radD.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radD.Location = new System.Drawing.Point(34, 312);
+            this.radD.Name = "radD";
+            this.radD.Size = new System.Drawing.Size(332, 55);
+            this.radD.TabIndex = 4;
+            this.radD.TabStop = true;
+            this.radD.Text = "D) Zoey, Cooper, Bella, and Teddy";
+            this.radD.UseVisualStyleBackColor = true;
+            // 
+            // radC
+            // 
+            this.radC.AutoSize = true;
+            this.radC.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radC.Location = new System.Drawing.Point(34, 252);
+            this.radC.Name = "radC";
+            this.radC.Size = new System.Drawing.Size(330, 55);
+            this.radC.TabIndex = 3;
+            this.radC.TabStop = true;
+            this.radC.Text = "C) Coco, Rocky, Daisy, and Pongo";
+            this.radC.UseVisualStyleBackColor = true;
+            // 
+            // radB
+            // 
+            this.radB.AutoSize = true;
+            this.radB.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radB.Location = new System.Drawing.Point(34, 192);
+            this.radB.Name = "radB";
+            this.radB.Size = new System.Drawing.Size(322, 55);
+            this.radB.TabIndex = 2;
+            this.radB.TabStop = true;
+            this.radB.Text = "B) Charlie, Max, Luna, and Bear";
+            this.radB.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(29, 39);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(344, 82);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "What are my dogs\' names?\r\n";
+            // 
+            // radA
+            // 
+            this.radA.AutoSize = true;
+            this.radA.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radA.Location = new System.Drawing.Point(34, 129);
+            this.radA.Name = "radA";
+            this.radA.Size = new System.Drawing.Size(342, 55);
+            this.radA.TabIndex = 0;
+            this.radA.TabStop = true;
+            this.radA.Text = "A) Tina, Spencer, Alice, and Buster";
+            this.radA.UseVisualStyleBackColor = true;
+            // 
             // btnSubmit
             // 
-            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(115, 429);
+            this.btnSubmit.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(115, 437);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(248, 41);
             this.btnSubmit.TabIndex = 1;
@@ -70,71 +128,14 @@
             this.btnSubmit.UseVisualStyleBackColor = true;
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
-            // radA
-            // 
-            this.radA.AutoSize = true;
-            this.radA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radA.Location = new System.Drawing.Point(34, 129);
-            this.radA.Name = "radA";
-            this.radA.Size = new System.Drawing.Size(143, 29);
-            this.radA.TabIndex = 0;
-            this.radA.TabStop = true;
-            this.radA.Text = "radioButton1";
-            this.radA.UseVisualStyleBackColor = true;
-            // 
-            // lblQuestion
-            // 
-            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuestion.Location = new System.Drawing.Point(29, 39);
-            this.lblQuestion.Name = "lblQuestion";
-            this.lblQuestion.Size = new System.Drawing.Size(344, 82);
-            this.lblQuestion.TabIndex = 1;
-            this.lblQuestion.Text = "What is the answer to this question?";
-            // 
-            // radB
-            // 
-            this.radB.AutoSize = true;
-            this.radB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radB.Location = new System.Drawing.Point(34, 182);
-            this.radB.Name = "radB";
-            this.radB.Size = new System.Drawing.Size(143, 29);
-            this.radB.TabIndex = 2;
-            this.radB.TabStop = true;
-            this.radB.Text = "radioButton1";
-            this.radB.UseVisualStyleBackColor = true;
-            // 
-            // radC
-            // 
-            this.radC.AutoSize = true;
-            this.radC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radC.Location = new System.Drawing.Point(34, 232);
-            this.radC.Name = "radC";
-            this.radC.Size = new System.Drawing.Size(143, 29);
-            this.radC.TabIndex = 3;
-            this.radC.TabStop = true;
-            this.radC.Text = "radioButton1";
-            this.radC.UseVisualStyleBackColor = true;
-            // 
-            // radD
-            // 
-            this.radD.AutoSize = true;
-            this.radD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radD.Location = new System.Drawing.Point(34, 282);
-            this.radD.Name = "radD";
-            this.radD.Size = new System.Drawing.Size(143, 29);
-            this.radD.TabIndex = 4;
-            this.radD.TabStop = true;
-            this.radD.Text = "radioButton1";
-            this.radD.UseVisualStyleBackColor = true;
-            // 
             // lblAnswer
             // 
             this.lblAnswer.BackColor = System.Drawing.Color.Transparent;
-            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Font = new System.Drawing.Font("Mistral", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAnswer.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblAnswer.Location = new System.Drawing.Point(464, 46);
+            this.lblAnswer.Location = new System.Drawing.Point(464, 51);
             this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(224, 32);
+            this.lblAnswer.Size = new System.Drawing.Size(238, 32);
             this.lblAnswer.TabIndex = 2;
             this.lblAnswer.Text = "The answer was:";
             this.lblAnswer.Visible = false;
@@ -142,26 +143,38 @@
             // lblAnswerLine2
             // 
             this.lblAnswerLine2.BackColor = System.Drawing.Color.Transparent;
-            this.lblAnswerLine2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerLine2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAnswerLine2.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblAnswerLine2.Location = new System.Drawing.Point(469, 78);
+            this.lblAnswerLine2.Location = new System.Drawing.Point(469, 89);
             this.lblAnswerLine2.Name = "lblAnswerLine2";
-            this.lblAnswerLine2.Size = new System.Drawing.Size(219, 154);
+            this.lblAnswerLine2.Size = new System.Drawing.Size(233, 71);
             this.lblAnswerLine2.TabIndex = 3;
-            this.lblAnswerLine2.Text = "[Answer]";
+            this.lblAnswerLine2.Text = "C) Coco, Rocky, Daisy, and Pongo";
             this.lblAnswerLine2.Visible = false;
             // 
             // btnNext
             // 
-            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNext.Location = new System.Drawing.Point(478, 247);
+            this.btnNext.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(478, 263);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(201, 63);
+            this.btnNext.Size = new System.Drawing.Size(215, 63);
             this.btnNext.TabIndex = 4;
             this.btnNext.Text = "Next question";
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Visible = false;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblAnswerLine3
+            // 
+            this.lblAnswerLine3.BackColor = System.Drawing.Color.Transparent;
+            this.lblAnswerLine3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerLine3.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblAnswerLine3.Location = new System.Drawing.Point(469, 172);
+            this.lblAnswerLine3.Name = "lblAnswerLine3";
+            this.lblAnswerLine3.Size = new System.Drawing.Size(233, 75);
+            this.lblAnswerLine3.TabIndex = 5;
+            this.lblAnswerLine3.Text = "[Answer feedback]";
+            this.lblAnswerLine3.Visible = false;
             // 
             // FormQuiz
             // 
@@ -169,7 +182,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(733, 498);
+            this.ClientSize = new System.Drawing.Size(732, 498);
+            this.Controls.Add(this.lblAnswerLine3);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.lblAnswerLine2);
             this.Controls.Add(this.lblAnswer);
@@ -196,6 +210,7 @@
         private System.Windows.Forms.Label lblAnswer;
         private System.Windows.Forms.Label lblAnswerLine2;
         private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblAnswerLine3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -30,21 +30,96 @@ namespace Topic_12_GUI_assignment_2
         private void FormQuiz_Load(object sender, EventArgs e)
         {
             this.BackgroundImage = Properties.Resources.Galaxy;
-            lblQuestion.Text = "";
         }
+
+        int round = 0, score = 0;
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            round++;
+
             lblAnswer.Visible = true;
             lblAnswerLine2.Visible = true;
+            lblAnswerLine3.Visible = true;
             btnNext.Visible = true;
 
-            lblAnswerLine2.Text = "[answer]";
+            if (round == 1)
+            {
+                if (radA.Checked)
+                {
+                    lblAnswerLine3.Text = "Daisy was almost named Alice, but you're still WRONG.";
+                }
+                else if (radB.Checked)
+                {
+                    lblAnswerLine3.Text = "These are 4 of the most common dog names of 2024. You're WRONG.";
+                }
+                else if (radC.Checked)
+                {
+                    score++;
+                    lblAnswerLine3.Text = "You were correct!";
+                }
+                else if (radD.Checked)
+                {
+                    lblAnswerLine3.Text = "These are all names of other dogs I've known. You're WRONG.";
+                }
+            }
+            else if (round == 2)
+            {
+                lblAnswerLine2.Text = "A) Pippin, Frodo, and Samwise";
+
+                if (radA.Checked)
+                {
+                    score++;
+                    lblAnswerLine3.Text = "Correct! Unfortunately, Meriadoc decided to chew on the cord for the heater.";
+                }
+                else if (radB.Checked)
+                {
+                    lblAnswerLine3.Text = "These were the names of the rabbits I had in the past.";
+                }
+                else if (radC.Checked)
+                {
+                    lblAnswerLine3.Text = "These are some common rabbit names; you were WRONG.";
+                }
+                else if (radD.Checked)
+                {
+                    lblAnswerLine3.Text = "These are names of famous rabbits; you were WRONG.";
+                }
+            }
+
         }
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            grpQuestion.Text = "Question 2";
+            lblAnswer.Visible = false;
+            lblAnswerLine2.Visible = false;
+            lblAnswerLine3.Visible = false;
+            btnNext.Visible = false;
+
+            if (round == 1)
+            {
+                grpQuestion.Text = "Question 2";
+                lblQuestion.Text = "What are my rabbits' names?";
+
+                radA.Text = "A) Pippin, Frodo, and Samwise";
+                radB.Text = "B) Pepsi, Nico, and Neville";
+                radC.Text = "C) Thor, Clover, and Willow";
+                radD.Text = "D) Thumper, Peter, and Bugs";
+            }
+            else if (round == 2)
+            {
+                grpQuestion.Text = "Congratulations!";
+                lblQuestion.Text = "You have reached the end of the quiz.";
+                radA.Text = "Final score: " + score;
+
+                radB.Visible = false;
+                radC.Visible = false;
+                radD.Visible = false;
+                lblAnswer.Visible = false;
+                lblAnswerLine2.Visible = false;
+                lblAnswerLine3.Visible = false;
+                btnNext.Visible = false;
+                btnSubmit.Visible = false;
+            }
         }
     }
 }
